43fdccbde2fe8846f76e50e1e40858850acac1f1
Amro256 <amro.e.sheikh@gmail.com>
2024-11-21 11:50:44 +0000

    Implemented Basic Controls & Cinemachine
    Player can now move around and jump using Unity's "new" Input System. Jump will need a raycast or bool for ground check. Also, Cinemachine was added for camera swap later on

--- Final Year Project/Assets/Scripts/PlayerMovement.cs
+++ Final Year Project/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.InputSystem; //Namespace for the next iput system
 public class PlayerMovement : MonoBehaviour
 {
+    //Variables to store Actions / Player Input referneces
+    InputAction moveAction;
+    InputAction jumpAction;
+    PlayerInput playerInput; 
+    
+    //Variables for Movement
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpPower = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Move"); //Find reference to the "Move" action
+        jumpAction = playerInput.actions.FindAction("Jump"); //Reference to the Move Action
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate() 
+    {
+        MovePlayer();
+        PlayerJump();
+        
+    }
+
+    void MovePlayer()
+    {
+        Vector2 moveDirection = moveAction.ReadValue<Vector2>();
+        rb.position += new Vector3(moveDirection.x, 0, moveDirection.y) * moveSpeed * Time.deltaTime;
+        
+    }
+
+    void PlayerJump()
+    {
+        if(jumpAction.IsPressed()) //if the jump button is pressed
+        {
+            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
     }
 }

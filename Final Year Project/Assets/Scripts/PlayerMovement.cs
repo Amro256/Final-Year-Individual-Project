9fd42f9c752e263993fba9fc55180228e0f69b71
Amro256 <amro.e.sheikh@gmail.com>
2024-12-01 12:18:07 +0000

    Updated the player controls

--- Final Year Project/Assets/Scripts/PlayerMovement.cs
+++ Final Year Project/Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 using UnityEngine.InputSystem; //Namespace for the next iput system
 public class PlayerMovement : MonoBehaviour
 {
-    //Variables to store Actions / Player Input referneces
-    InputAction moveAction;
-    InputAction jumpAction;
-    PlayerInput playerInput; 
-    private LayerMask groundmask;
-    [SerializeField] bool isGrounded = false;
+
+    private PlayerInput playerInput;  //Reference to the player Input 
+
 
     //Variables for Movement
-    [SerializeField] Rigidbody rb;
+    [SerializeField] private Rigidbody rb;
     [SerializeField] float moveSpeed = 5f;
+    Vector2 moveDirection;
     [SerializeField] float jumpPower = 5f;
+    [SerializeField] bool isGrounded;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] GameObject castPostion;
 
-    void OnEnable()
+
+    void Awake() //Happens just before start
     {
-        playerInput.actions.Enable(); //Will enable the controls if the gameObject is enable 
+         rb = GetComponent<Rigidbody>();
+         playerInput = GetComponent<PlayerInput>();
+         playerInput.actions.Enable(); //Enables the controls
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+    
     }
 
     void OnDisable() 
@@ -26,58 +36,46 @@ public class PlayerMovement : MonoBehaviour
          playerInput.actions.Disable(); //Will disable the controls is the gameObejct is disabled
     }
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb.GetComponent<Rigidbody>();
-        playerInput = GetComponent<PlayerInput>();
-        moveAction = playerInput.actions.FindAction("Move"); //Find reference to the "Move" action
-        jumpAction = playerInput.actions.FindAction("Jump"); //Reference to the Move Action
-        groundmask = LayerMask.GetMask("Ground"); //For the raycast to detect the correct layer mask for player jumping
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        CheckGround(); //Call the CheckGround method to keep firing raycast
     }
 
     private void FixedUpdate() 
     {
-        MovePlayer();
-        CheckGround();
-        PlayerJump();
-    }
-
-    
-
-    void MovePlayer()
-    {
-        Vector2 moveDirection = moveAction.ReadValue<Vector2>();
         rb.position += new Vector3(moveDirection.x, 0, moveDirection.y) * moveSpeed * Time.deltaTime;
-    }
-
-    void PlayerJump()
-    {
-        if(jumpAction.IsPressed() ) //if the jump button is pressed
-        {
-            //isGrounded = false;
-            Debug.Log("Jump!");
-            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);   
-            
-        } 
-            
-    }
-
-     void CheckGround()
-    {
         
+    }
+
+    public void OnMove (InputAction.CallbackContext context) //Changed to using the Invoke Unity behaviour, so this method will need to take in a CallBackContext to trigger this method
+    {
+        //Using "Invoke" Unity events does not take continuous hold into account
+
+        moveDirection = context.ReadValue<Vector2>(); //Works but it need to be called every frame to update movement 
+
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+
+        if(context.performed && isGrounded) // Will only jump is the player presses the jump button AND if the player is grounded
+        {
+            rb.AddForce(new Vector3(0,jumpPower,0 ), ForceMode.Impulse);
+            isGrounded = false;
+        }
+   
+    }
+
+    //Method that fires of a raycast to check if the player is colliding with the Ground layer mask
+     void CheckGround() 
+    {
         RaycastHit hit;
 
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, out hit, 1f, groundmask);
+        isGrounded = Physics.Raycast(castPostion.transform.position, Vector3.down, out hit, 0.5f, groundLayer);
 
-        Debug.DrawRay(rb.position, Vector3.down, Color.red, 3f);
+        Debug.DrawRay(castPostion.transform.position, Vector3.down, Color.red, 0.5f);
         Debug.Log ("Ray fired and hit ground");
 
     }

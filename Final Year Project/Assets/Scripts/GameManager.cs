4780a932da1fba17196a1d2eb05eed0613d995e4
Amro256 <amro.e.sheikh@gmail.com>
2025-02-08 11:46:13 +0000

    Adding Scene Transition
    In order to mask abrupt cuts and load times, I added a basic scene transition from the main menu to the prototype scene. This will further refined when other menus have been added in

--- Final Year Project/Assets/Scripts/GameManager.cs
+++ Final Year Project/Assets/Scripts/GameManager.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance; //Making this script a singleton
+
+    //variables
+    [SerializeField] Animator transitionAnim; //Reference to the animation
+    [SerializeField] string sceneName; //Using a string to change scene so it's not hard coded and it'll be easier to switch while test
+   
+    void Awake() 
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }    
+        else 
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.I)) //for testing purposes 
+        {
+            //Play animation & load scene in the background 
+           StartCoroutine(LoadScene());
+            
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("isTrigger");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadSceneAsync(sceneName);
         
     }
 }
